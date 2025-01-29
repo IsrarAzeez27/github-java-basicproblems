c531ba06fbf72b94688becb33a7b3b17685676e6
Israr Azeez <194682181+IsrarAzeez27@users.noreply.github.com>
2025-01-30 01:14:01 +0530

    Multiplication Table commit

---
+++ src/main/java/MultiplicationTable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        for(int i=0; i<10; i++){
+            System.out.println(number + " X " + (i+1) + " = " + (number * (i+1)));
+        }
+    }
+}
