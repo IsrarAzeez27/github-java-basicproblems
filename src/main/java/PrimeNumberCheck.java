fa33a6c2fbfe8c6df6923c5584d8875e3f4f397c
Israr Azeez <194682181+IsrarAzeez27@users.noreply.github.com>
2025-07-26 12:12:31 +0530

    Check Prime Number

---
+++ src/main/java/PrimeNumberCheck.java
@@ -0,0 +1,24 @@
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+
+        int num = 29; // Change this number to test others
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
