f7b54f6807a76acc930cb541760e5d537e5d0403
Israr Azeez <194682181+IsrarAzeez27@users.noreply.github.com>
2025-09-16 04:28:28 +0530

    New practice problems

---
+++ src/main/java/ReverseStringPractice4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReverseStringPractice4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inout a string to reverse : ");
+        char[] letters = sc.nextLine().toCharArray();
+
+        System.out.println("The Reverse string is : ");
+        for(int i = letters.length-1; i>=0; i--){
+            System.out.print(letters[i]);
+        }
+        System.out.print("\n");
+    }
+}
