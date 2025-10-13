7b0eb87656f6e6e38ad5841b735b98e88fba919c
Israr Azeez <194682181+IsrarAzeez27@users.noreply.github.com>
2025-10-14 03:44:08 +0530

    Largest of 2 numbers without using IF condition

---
+++ src/main/java/LargestNumberWithoutIfCondition.java
@@ -0,0 +1,12 @@
+public class LargestNumberWithoutIfCondition {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 30;
+
+        int largest = a>b?a:b;
+        // int largest = Math.max(a, b);
+
+        System.out.println(largest);
+    }
+}
